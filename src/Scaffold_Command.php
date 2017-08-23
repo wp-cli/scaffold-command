41b614abe13a47768306d594111f5a9d935bff85
Umangvaghela <umang.multidots@gmail.com>
2017-08-23 13:21:11 +0530

    add three rule in Scaffold_Command.php

--- src/Scaffold_Command.php
+++ src/Scaffold_Command.php
@@ -882,24 +882,27 @@ class Scaffold_Command extends WP_CLI_Command {
 
 	private function pluralize( $word ) {
 		$plural = array(
-			'/(quiz)$/i'               => '\1zes',
-			'/^(ox)$/i'                => '\1en',
-			'/([m|l])ouse$/i'          => '\1ice',
-			'/(matr|vert|ind)ix|ex$/i' => '\1ices',
-			'/(x|ch|ss|sh)$/i'         => '\1es',
-			'/([^aeiouy]|qu)ies$/i'    => '\1y',
-			'/([^aeiouy]|qu)y$/i'      => '\1ies',
-			'/(hive)$/i'               => '\1s',
-			'/(?:([^f])fe|([lr])f)$/i' => '\1\2ves',
-			'/sis$/i'                  => 'ses',
-			'/([ti])um$/i'             => '\1a',
-			'/(buffal|tomat)o$/i'      => '\1oes',
-			'/(bu)s$/i'                => '1ses',
-			'/(alias|status)/i'        => '\1es',
-			'/(octop|vir)us$/i'        => '1i',
-			'/(ax|test)is$/i'          => '\1es',
-			'/s$/i'                    => 's',
-			'/$/'                      => 's'
+			'/(quiz)$/i'                     => '\1zes',
+			'/^(ox)$/i'                      => '\1en',
+			'/([m|l])ouse$/i'                => '\1ice',
+			'/(matr|vert|ind)ix|ex$/i'       => '\1ices',
+			'/(x|ch|ss|sh)$/i'               => '\1es',
+			'/([^aeiouy]|qu)ies$/i'          => '\1y',
+			'/([^aeiouy]|qu)y$/i'            => '\1ies',
+			'/(hive)$/i'                     => '\1s',
+			'/(?:([^f])fe|([lr])f)$/i'       => '\1\2ves',
+			'/(shea|lea|loa|thie)f$/i'       => '\1ves',
+			'/sis$/i'                        => 'ses',
+			'/([ti])um$/i'                   => '\1a',
+			'/(tomat|potat|ech|her|vet)o$/i' => '\1oes',
+			'/(buffal|tomat)o$/i'            => '\1oes',
+			'/(bu)s$/i'                      => '1ses',
+			'/(alias|status)/i'              => '\1es',
+			'/(octop|vir)us$/i'              => '1i',
+			'/(ax|test)is$/i'                => '\1es',
+			'/(us)$/i'                       => '\1es',
+			'/s$/i'                          => 's',
+			'/$/'                            => 's'
 		);
 
 		$uncountable = array( 'equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep' );
