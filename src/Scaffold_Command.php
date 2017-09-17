b3dd9c1c712c04be568fd02f55f93abac8f789a5
fumikito <guy@hametuha.com>
2017-09-17 15:54:58 +0900

    plularize() depends on Inflector

--- src/Scaffold_Command.php
+++ src/Scaffold_Command.php
@@ -2,6 +2,7 @@
 
 use WP_CLI\Utils;
 use WP_CLI\Process;
+use Doctrine\Common\Inflector\Inflector;
 
 /**
  * Generate code for post types, taxonomies, plugins, child themes, etc.
@@ -881,59 +882,7 @@ class Scaffold_Command extends WP_CLI_Command {
 	}
 
 	private function pluralize( $word ) {
-		$plural = array(
-			'/(quiz)$/i'               => '\1zes',
-			'/^(ox)$/i'                => '\1en',
-			'/([m|l])ouse$/i'          => '\1ice',
-			'/(matr|vert|ind)ix|ex$/i' => '\1ices',
-			'/(x|ch|ss|sh)$/i'         => '\1es',
-			'/([^aeiouy]|qu)ies$/i'    => '\1y',
-			'/([^aeiouy]|qu)y$/i'      => '\1ies',
-			'/(hive)$/i'               => '\1s',
-			'/(?:([^f])fe|([lr])f)$/i' => '\1\2ves',
-			'/sis$/i'                  => 'ses',
-			'/([ti])um$/i'             => '\1a',
-			'/(buffal|tomat)o$/i'      => '\1oes',
-			'/(bu)s$/i'                => '1ses',
-			'/(alias|status)/i'        => '\1es',
-			'/(octop|vir)us$/i'        => '1i',
-			'/(ax|test)is$/i'          => '\1es',
-			'/s$/i'                    => 's',
-			'/$/'                      => 's',
-		);
-
-		$uncountable = array( 'equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep' );
-
-		$irregular = array(
-			'person' => 'people',
-			'man'    => 'men',
-			'woman'  => 'women',
-			'child'  => 'children',
-			'sex'    => 'sexes',
-			'move'   => 'moves',
-		);
-
-		$lowercased_word = strtolower( $word );
-
-		foreach ( $uncountable as $_uncountable ) {
-			if ( substr( $lowercased_word, ( - 1 * strlen( $_uncountable ) ) ) == $_uncountable ) {
-				return $word;
-			}
-		}
-
-		foreach ( $irregular as $_plural => $_singular ) {
-			if ( preg_match( '/(' . $_plural . ')$/i', $word, $arr ) ) {
-				return preg_replace( '/(' . $_plural . ')$/i', substr( $arr[0], 0, 1 ) . substr( $_singular, 1 ), $word );
-			}
-		}
-
-		foreach ( $plural as $rule => $replacement ) {
-			if ( preg_match( $rule, $word ) ) {
-				return preg_replace( $rule, $replacement, $word );
-			}
-		}
-
-		return false;
+		return Inflector::pluralize( $word );
 	}
 
 	protected function extract_args( $assoc_args, $defaults ) {
