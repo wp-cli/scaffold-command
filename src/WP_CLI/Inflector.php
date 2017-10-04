26f95f87e8ad1d0afcbba1e324fecfafd35de2ec
miya0001 <miya@wpist.me>
2017-10-04 22:54:06 +0900

    change namespece of the Inflector

--- src/WP_CLI/Inflector.php
+++ src/WP_CLI/Inflector.php
@@ -17,7 +17,7 @@
  * <http://www.doctrine-project.org>.
  */
 
-namespace Doctrine\Common\Inflector;
+namespace WP_CLI\Inflector;
 
 /**
  * Doctrine inflector has static methods for inflecting text.
@@ -404,7 +404,7 @@ class Inflector
 
         if (preg_match('/(.*)\\b(' . self::$plural['cacheIrregular'] . ')$/i', $word, $regs)) {
             self::$cache['pluralize'][$word] = $regs[1] . substr($word, 0, 1) . substr(self::$plural['merged']['irregular'][strtolower($regs[2])], 1);
-            
+
             return self::$cache['pluralize'][$word];
         }
 
@@ -457,7 +457,7 @@ class Inflector
 
         if (preg_match('/(.*)\\b(' . self::$singular['cacheIrregular'] . ')$/i', $word, $regs)) {
             self::$cache['singularize'][$word] = $regs[1] . substr($word, 0, 1) . substr(self::$singular['merged']['irregular'][strtolower($regs[2])], 1);
-            
+
             return self::$cache['singularize'][$word];
         }
 
