b3483e2596ec640f8756f68598775c65a62d3b18
Alain Schlesser <alain.schlesser@gmail.com>
2018-04-20 00:20:05 +0200

    Update scaffolded files

--- bin/test.sh
+++ bin/test.sh
@@ -8,6 +8,10 @@ then
 	phpunit
 fi
 
+if [ $WP_VERSION = "latest" ]; then
+	export WP_VERSION=$(curl -s https://api.wordpress.org/core/version-check/1.7/ | jq -r ".offers[0].current")
+fi
+
 # Run the functional tests
 BEHAT_TAGS=$(php utils/behat-tags.php)
 behat --format progress $BEHAT_TAGS --strict
