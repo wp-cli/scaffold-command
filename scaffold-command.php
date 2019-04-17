97e237804759660f6522d2576f7fc8305f8c16ec
Alain Schlesser <alain.schlesser@gmail.com>
2019-04-17 15:05:33 +0200

    Prefix variables in global namespace

--- scaffold-command.php
+++ scaffold-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_scaffold_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_scaffold_autoloader ) ) {
+	require_once $wpcli_scaffold_autoloader;
 }
 
 WP_CLI::add_command( 'scaffold', 'Scaffold_Command' );
