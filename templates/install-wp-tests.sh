0328044ba9de810241d10c80dc7a8da6ee2e92e4
Ted Stresen-Reuter <ted@secret-source.eu>
2020-01-24 14:29:01 +0000

    Fixed 228 - init script can be used repeatedly
    Fixed in issue in which the init script would fail if the database was already created.

--- templates/install-wp-tests.sh
+++ templates/install-wp-tests.sh
@@ -147,6 +147,7 @@ install_db() {
 	fi
 
 	# create database
+	mysqladmin drop $DB_NAME -f --user="$DB_USER" --password="$DB_PASS"$EXTRA > /dev/null 2>&1
 	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 }
 
