70e5dccec0ba116c9be52ba3ba9b97630d78d7c1
backroot <kotaro_saito@noah.jp>
2021-02-28 10:28:16 +0900

    Add phpcs rules for wp

---
+++ templates/install-wp-phpcs-rules.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+composer require dealerdirect/phpcodesniffer-composer-installer
+composer require --dev phpcompatibility/phpcompatibility-wp:*
+composer require --dev wp-coding-standards/wpcs:*
+composer require --dev automattic/vipwpcs:*
